b4829bea91168a75d9f073a87f103d0d698c12fe
unknown <rekhasei94@gmail.com>
2019-11-05 14:47:16 +0530

    ok

---
+++ javafundamentals/src/javafundamentals/HelloWorld.java
@@ -0,0 +1,12 @@
+package javafundamentals;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<3;i++)
+		System.out.println("hello hcl " + i);
+
+	}
+ 
+}
