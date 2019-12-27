d01ca152cf08514f04caad3dd87b4e78599949c9
rekhasri1994 <57347063+rekhasri1994@users.noreply.github.com>
2019-12-27 13:49:03 +0530

    Add files via upload

---
+++ IO/A.java
@@ -0,0 +1,12 @@
+package IO;
+
+import java.util.Scanner;
+class A
+{
+	public static void main(String[] args) 
+	{
+      Scanner sc=new Scanner(System.in);
+      int i=sc.nextInt();
+      System.out.println(i);
+    }
+}
---
+++ IO/AcceptMe.java
@@ -0,0 +1,21 @@
+package IO;
+
+import java.util.Scanner;
+
+public class AcceptMe {
+
+	public static void main(String[] args) 
+	
+	{
+		Scanner sc=new Scanner(System.in);
+		String empname=sc.next();
+		int empid=sc.nextInt();
+		int empsal=sc.nextInt();
+		System.out.println(empname);
+		System.out.println(empid);
+		System.out.println(empsal);
+		
+
+	}
+
+}
